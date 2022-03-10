74fe2115a21fd5f464a7d5e9f1c340437cc9e3a7
Julia <juliac.kraus@yahoo.de>
2022-03-09 20:57:49 -0400

    2nd copy

---
+++ scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $(".hamburger-button").click(function() {
+        $(this).toggleClass("active");
+        $(".mobile-menu").fadeToggle()
+    });
+});
+
+lightGallery(document.getElementById('lightgallery'), {
+    speed: 500,
+    download: false
+});
